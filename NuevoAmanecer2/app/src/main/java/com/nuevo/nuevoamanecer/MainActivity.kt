dd017197a8bf666ff9b89a507975d8240d596423
cuiltyv <a01721636@itesm.mx>
2023-10-12 08:39:17 -0600

    Botton Color y Otros Cambios
    Botton Cambia de Color y si le picas a la imagen se mueve a nueva pagina

--- NuevoAmanecer2/app/src/main/java/com/nuevo/nuevoamanecer/MainActivity.kt
+++ NuevoAmanecer2/app/src/main/java/com/nuevo/nuevoamanecer/MainActivity.kt
@@ -2,10 +2,26 @@ package com.nuevo.nuevoamanecer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Change to the MainActivity2
+        val btnImg1 = findViewById(R.id.imageButton3) as ImageButton
+
+
+
+        btnImg1.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
