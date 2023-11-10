bacbec57db3d3578495c5f6ec62614e4d3c494e3
Tellotello <a01721735@tec.mx>
2023-11-10 08:49:07 -0600

    Juntar con juego de cherry

---
+++ NuevoAmanecer2/app/src/main/java/com/nuevo/nuevoamanecer/ActivityGame4.kt
@@ -0,0 +1,11 @@
+package com.nuevo.nuevoamanecer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityGame4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game4)
+    }
+}
